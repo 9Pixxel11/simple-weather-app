3144ef92fba66b6c449d13e9b5538ef2fc908ec3
Cris Vilares <cristofo.vilares@gmail.com>
2020-04-03 00:29:58 +0200

    build searchbox component and added it to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
+import
+
 <template>
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
-      <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
-          placeholder="Suche..."
-          v-model="query"
-          @keypress="fetchWeather"
-        />
-      </div>
+      
+      <SearchBox @weather-result="getWeatherResult"/>
 
       <!-- conditional: if weather response is undefined don't show any result -->
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
@@ -24,34 +19,23 @@
         </div>
 
       </div>
-
     </main>
   </div>
 </template>
 
 <script>
 
+import SearchBox from './components/SearchBox';
+
 export default {
+  components: { SearchBox },
   name: 'App',
   data() {
-    return  {
-      api_key: '36cf36c5cb03aac232269fda01ee4db2',
-      url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: '',
-      weather: {}
-    }
+    return  { weather: {} }
   },
   methods: {
-    fetchWeather (e) {
-      if(e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&lang=de&appid=${this.api_key}`)
-          .then(res => {
-            return res.json();
-          }).then(this.setResults);
-      }
-    },
-    setResults (results) {
-      this.weather = results;
+    getWeatherResult: function(result) {
+      this.weather = result;
     },
     dateBuilder () {
       let d = new Date();
